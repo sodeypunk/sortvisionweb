650c961b8037ee4dd8c4f2fc9552824c2f9388d0
soda heng <soda.heng@gmail.com>
2017-01-11 21:24:57 -0600

    Moved files into bibcommander folder

--- application/views/pages/price.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: soda
- * Date: 2/24/16
- * Time: 10:50 PM
- */
--- application/views/templates/footer.php
+++
@@ -1,5 +0,0 @@
-        <footer class="footer">
-            <p>&copy; 2015-2016 SortVISION, Inc.</p>
-        </footer>
-    </body>
-</html>
